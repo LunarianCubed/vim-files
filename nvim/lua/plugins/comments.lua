4d1b36702f4147a1537fbf7de62258dea9eb5501
Lunarian3 <me@lunarian3.com>
2025-04-27 16:29:24 -0500

    Update plugins/comments:     add support for sml/sig/grm/lex filetype

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -12,6 +12,14 @@ return {
     -- Manual keymaps
     local api = require("Comment.api")
     vim.keymap.set("n", "<gc>", api.toggle.linewise.current, { desc = "Toggle comment line" })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "sml", "sig", "grm", "lex" },
+      callback = function()
+        vim.opt_local.commentstring = "(* %s *)"
+      end,
+    })
+
   end,
 }
 
