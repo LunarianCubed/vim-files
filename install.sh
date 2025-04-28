f1bab060b5bb80f6e0a0049627cee9562e2b3a68
lunarian3 <me@lunarian3.com>
2025-04-27 20:10:13 -0500

    add simple install script

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+
+usage(){
+    echo "Usage: $0 [nvim|vim]"
+}
+
+backup(){
+    local target=$1
+    if [ -e "$target" ]; then
+        local bak_target="${target}.bak"
+        echo "[INFO] $target exists, moving to $bak_target"
+        mv "$target" "$bak_target"
+    fi
+}
+
+install_neovim() {
+    local src_dir="./nvim"
+    local dest_dir="$HOME/.config/nvim/"
+    mkdir -p "$HOME/.config/"
+    backup "$dest_dir"
+    echo "[INFO] Copying $src_dir -> $dest_dir"
+    cp -r "$src_dir" "dest_dir"
+}
+
+install_vim() {
+    local src_file="./vim/vimrc"
+    local dest_file="$HOME/.vimrc"
+
+    backup "$dest_dir"
+    echo "[INFO] Copying $src_dir -> $dest_dir"
+    cp -r "$src_dir" "dest_dir"
+}
+
+if [[ $# -ne 1 ]]; then
+    usage
+fi
+
+case "$1" in
+    nvim)  
+        install_nvim
+    ;;
+    vim) 
+        install_vim
+    ;;
+    *) 
+        usage
+    ;;
+esac
+
+echo "[DONE] Installation complete."
