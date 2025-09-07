055e563b77ca7ab3a65441ade6e9b521d213b89a
Lunarian3 <me@lunarian3.com>
2025-09-06 20:30:14 -0500

    add sync.sh:overright repository with local user nvim config

---
+++ nvim/sync.sh
@@ -0,0 +1,11 @@
+!#/bin/bash
+
+USERCONFIG="$HOME/.config/nvim/"
+
+
+if [[ -f "$USERCONFIG/init.lua" && -d "$USERCONFIG/lua"]]; then
+    echo "Copying User config -> $(pwd)"
+    cp -f "$USERCONFIG/init.lua" "$CWD/init.lua"
+    cp -f "$USERCONFIG/lua" "$CWD/lua"
+else
+    echo "ERROR:USERCONFIG IS NOT COMPLETED. Stopping."
