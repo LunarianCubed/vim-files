c1642e30db7a00e9f2ddee04895846f789d3e194
Lunarian3 <me@lunarian3.com>
2025-09-06 20:40:29 -0500

    fix sync.sh

--- nvim/sync.sh
+++ nvim/sync.sh
@@ -7,7 +7,7 @@ CWD="$(pwd)"
 if [[ -f "$USERCONFIG/init.lua" && -d "$USERCONFIG/lua" ]]; then
     echo "Copying User config -> $CWD"
     cp -f "$USERCONFIG/init.lua" "$CWD/init.lua"
-    cp -rf "$USERCONFIG/lua" "$CWD/lua"
+    cp -rf "$USERCONFIG/lua/." "$CWD/lua/"
 else
     echo "ERROR:USERCONFIG IS NOT COMPLETED. Stopping."
 fi
