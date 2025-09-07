f848482e47913a5fdf28dfafffc3d82761ff7b1c
Lunarian3 <me@lunarian3.com>
2025-09-06 20:34:49 -0500

    fix sync.sh syntax

--- nvim/sync.sh
+++ nvim/sync.sh
@@ -1,11 +1,12 @@
-!#/bin/bash
+#!/bin/bash
 
-USERCONFIG="$HOME/.config/nvim/"
+USERCONFIG="$HOME/.config/nvim"
 
 
-if [[ -f "$USERCONFIG/init.lua" && -d "$USERCONFIG/lua"]]; then
+if [[ -f "$USERCONFIG/init.lua" && -d "$USERCONFIG/lua" ]]; then
     echo "Copying User config -> $(pwd)"
     cp -f "$USERCONFIG/init.lua" "$CWD/init.lua"
     cp -f "$USERCONFIG/lua" "$CWD/lua"
 else
     echo "ERROR:USERCONFIG IS NOT COMPLETED. Stopping."
+fi
