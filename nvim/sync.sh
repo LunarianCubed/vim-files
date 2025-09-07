3ece5bb5bb471f946c73fbdcb07ea8d56c5eeec0
Lunarian3 <me@lunarian3.com>
2025-09-06 20:36:39 -0500

    fix sync.sh

--- nvim/sync.sh
+++ nvim/sync.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 USERCONFIG="$HOME/.config/nvim"
+CWD="$(pwd)"
 
 
 if [[ -f "$USERCONFIG/init.lua" && -d "$USERCONFIG/lua" ]]; then
-    echo "Copying User config -> $(pwd)"
+    echo "Copying User config -> $CWD"
     cp -f "$USERCONFIG/init.lua" "$CWD/init.lua"
-    cp -f "$USERCONFIG/lua" "$CWD/lua"
+    cp -rf "$USERCONFIG/lua" "$CWD/lua"
 else
     echo "ERROR:USERCONFIG IS NOT COMPLETED. Stopping."
 fi
